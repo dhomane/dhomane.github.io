bea903812701b7fc44542edbaf2b29e6337d1a7a
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2023-01-25 16:48:47 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/dhomane/dhomane.github.io.git";
 
 ghpages.publish(
   pathname,
